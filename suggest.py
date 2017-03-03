6d74d73f31d53dcdb38cc69aa7775bb4b0f83717
Elias Dorneles <eliasdorneles@gmail.com>
2017-03-03 14:28:08 -0500

    added script to lookup words

---
+++ suggest.py
@@ -0,0 +1,22 @@
+import re
+from trie import Trie
+
+
+def parse_words(text):
+    return re.findall(r'\w+', text.lower())
+
+
+with open('big.txt') as f:
+    words = parse_words(f.read())
+
+
+def trie_with_all_words():
+    t = Trie()
+    for w in words:
+        t.add(w)
+    return t
+
+
+if __name__ == '__main__':
+    t = trie_with_all_words()
+    print(t.lookup('recur'))
